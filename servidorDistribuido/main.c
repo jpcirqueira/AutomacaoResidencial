a41011db9dac724432bdee8e289fd1c693ae6999
jpcirqueira <jpcirqueira81@gmail.com>
2021-04-12 22:19:52 -0300

    ajustes servidor distribuido.

--- servidorDistribuido/main.c
+++ servidorDistribuido/main.c
@@ -6,30 +6,34 @@
 #include <stdlib.h>
 #include "./bme/bme.h"
 #include "./gpio/gpio.h"
+#include "./servidorTcp/servidor_tcp.h"
 
 #define LAMPADA_01 0
 #define LAMPADA_02 1
-#define LAMPADA_03 13
-#define LAMPADA_04 15
-#define ARCONDICIONADO_01 33
-#define ARCONDICIONADO_02 35
+#define LAMPADA_03 2
+#define LAMPADA_04 3
+#define ARCONDICIONADO_01 23
+#define ARCONDICIONADO_02 24
 #define SENSOR_PRESENCA_01 6
-#define SENSOR_PRESENCA_02 37
-#define SENSOR_ABERTURA_01 21
-#define SENSOR_ABERTURA_02 31
-#define SENSOR_ABERTURA_03 26
-#define SENSOR_ABERTURA_04 27
-#define SENSOR_ABERTURA_05 28
-#define SENSOR_ABERTURA_06 29
+#define SENSOR_PRESENCA_02 25
+#define PORTA_COZINHA 21
+#define JANELA_COZINHA 22
+#define PORTA_SALA 26
+#define JANELA_SALA 27
+#define JANELA_QUARTO_01 28
+#define JANELA_QUARTO_02 29
 
 int bmeErro =0;
 
 int main(){
+  servidor();
   // gpio_liga(LAMPADA_01);
   // sleep(5);
   // gpio_desliga(LAMPADA_01);
   // int teste = ler_gpio(SENSOR_PRESENCA_01);
   // printf("%d\n",teste);
+
+
   // while (1)
   // {   
   //   float te = bme();
@@ -37,5 +41,4 @@ int main(){
   // }
 return 0;
 }
-
 
